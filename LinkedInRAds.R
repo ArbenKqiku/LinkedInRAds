2be1305d954d569b1a3c114f5c99325149b8985f
ArbenKqiku <arben.kqiku@gmail.com>
2020-10-25 09:45:34 +0100

    Added possibility to add an image to a post

--- LinkedInRAds.R
+++ LinkedInRAds.R
@@ -46,15 +46,45 @@ result = httr::POST(post_request_access_token_url) %>%
 # please write access token manually otherwise you'll lose it next time you'll reconnect
 access_token = "insert_your_access_token_manually"
 
-# 6 Get the information of your personal LinkedIn account ----
 headers = c(
     `Authorization` = str_c("Bearer ", access_token)
 )
 
+# 6 Get the information of your personal LinkedIn account ----
 personal_li_info = GET(url = 'https://api.linkedin.com/v2/me', add_headers(.headers=headers)) %>% 
     content()
 
-# 7 Prepare variables to make post request to share a post ----
+# 7 Prepare a share of an image -----
+# Here you are basically getting a place where you can then store your image that you can use
+# for subsequent posts
+image_share_body = list(registerUploadRequest = 
+                            list(recipes = list("urn:li:digitalmediaRecipe:feedshare-image"),
+                                 owner = str_c("urn:li:person:", personal_li_info$id),
+                                 serviceRelationships = list(list(
+                                     relationshipType = "OWNER",
+                                     identifier = "urn:li:userGeneratedContent"
+                                 )))) %>% 
+    
+    toJSON(auto_unbox = TRUE, pretty = TRUE)
+
+post_URL = "https://api.linkedin.com/v2/assets?action=registerUpload"
+
+image_response = POST(url = post_URL, body = image_share_body, add_headers(.headers = headers)) %>% 
+    content()
+
+# retrieve useful variables for later
+# the upload URL is the URL that we are going to use in the next request
+upload_URL = image_response$value$uploadMechanism$com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest$uploadUrl
+asset = image_response$value$asset
+
+# 8 Post an image on LinkedIn that we can use later to add into a post ----
+# create image upload
+image_to_upload = upload_file("nasa.jpeg")
+
+# post the image to use later 
+image_post_request = POST(url = upload_URL, body = image_to_upload, add_headers(.headers = headers))
+
+# 9 Prepare variables to make post request to share a post ----
 # URL of the request
 post_URL = "https://api.linkedin.com/v2/ugcPosts"
 
@@ -62,9 +92,18 @@ post_URL = "https://api.linkedin.com/v2/ugcPosts"
 # here you'll find the full documentation: https://docs.microsoft.com/en-gb/linkedin/consumer/integrations/self-serve/share-on-linkedin
 personal_urn = str_c("urn:li:person:", personal_li_info$id)
 lifecycleState = "PUBLISHED"
-text = "🤩 Hey, this post was created by using R and the LinkedIn User Generated Content (UGC) API"
-media_category = "NONE"
+text = str_glue("✅ Hey, this post was created by using R and the LinkedIn API. This time, I also added an image.
+                
+                If you want to see how I did it, here is the source code: https://github.com/ArbenKqiku/LinkedInRAds/blob/main/LinkedInRAds.R",
+                
+                "\n\n✅ Follow me for more tips on #datascience, #digitalmarketing & #programming.
+                
+                #r #rstats #dailycoding #arbentips #api #linkedin #apidevelopment")
+media_category = "IMAGE"
 visibility = "PUBLIC"
+status = "READY"
+description_text = "this is an image description"
+title_text = "this is an image title text"
 
 # build body
 post_request_body = list(
@@ -72,10 +111,17 @@ post_request_body = list(
     lifecycleState = lifecycleState,
     specificContent = list(
         com.linkedin.ugc.ShareContent =
-                    list(shareCommentary =
-                             list(text = text),
-                         shareMediaCategory = media_category
-                    )
+            list(shareCommentary =
+                     list(text = text),
+                 shareMediaCategory = media_category,
+                 media = list(
+                     list(status = status,
+                          description = list(
+                              text = description_text),
+                          media = asset,
+                          title = list(text = title_text))
+                 )
+            )
     ),
     visibility = list(
         com.linkedin.ugc.MemberNetworkVisibility = visibility
@@ -83,6 +129,6 @@ post_request_body = list(
 ) %>% 
     toJSON(auto_unbox = TRUE, pretty = TRUE)
 
-# 8 Make post request to share a post ----
-POST(url = post_URL, body = post_request_body, httr::add_headers(.headers = headers))
-
+# 10 Make post request to share a post ----
+POST(url = post_URL, body = post_request_body, httr::add_headers(.headers = headers)) %>% 
+    content()
